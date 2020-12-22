e0ef89db08ce7b70ddfc3878ea9e5da63548f465
helderramosoliveira <helder.ramos.oliveira@gmail.com>
2020-12-22 12:00:31 -0300

    pos revisão

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,124 @@
 <template>
   <div id="app">
-    <nav id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <h1>teste</h1>
-    </nav>
-    <router-view/>
-   </div>
+    <aside id="barraLateral">
+      <img id="logo" src="/img/logo.png" />
+      <nav id="menuLateral">
+        <ul id="menuList">
+          <li v-for="(link, idx) in links" :key="idx">
+            <router-link :to="link.path"> <!-- no view usa "router-link to" no lugar de <a href!-->
+                <img :src="link.img" :alt="link.title" /> {{ link.title }}
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+    <router-view />
+  </div>
 </template>
 
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  data() {
+    return {
+      links: [
+        {
+          title: 'Dashboard',
+          img: '/img/dashboard.png',
+          path: '/',
+        },
+        {
+          title: 'My Taks',
+          img: '/img/my-task.png',
+          path: '/',
+        },
+        {
+          title: 'Message',
+          img: '/img/message.png',
+          path: '/',
+        },
+        {
+          title: 'Notification',
+          img: '/img/notification.png',
+          path: '/',
+        },
+        {
+          title: 'Statistik',
+          img: '/img/statistik.png',
+          path: '/',
+        },
+        {
+          title: 'My File',
+          img: '/img/my-file.png',
+          path: '/',
+        },
+      ],
+    };
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@charset "UTF-8";
+@import url("https://fonts.googleapis.com/css2?family=Sniglet&display=swap");
+body {
+  font-family: "Sniglet", cursive;
+  background-color: #eef1f3;
+  background-image: linear-gradient(to bottom, #eef1f3, #fbe6d6, #f3d8cd);
+  background-attachment: fixed;
 }
 
-#nav {
-  padding: 30px;
+aside#barraLateral {
+  background-color: #f7f7f7;
+  width: 200px;
+  height: 768px;
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
+  margin: 10px;
+  margin-right: 0px;
+  float: left;
+  opacity: 0.8;
+  /*border: solid;*/
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+nav#menuLateral {
+  display: block;
+  margin-left: 40px;
+  /*border: solid;*/
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+img#logo {
+  /*color: #5A5858; funciona nao*/
+  margin-left: 20px;
+  margin-top: 9px;
+}
+
+nav#menuLateral ul {
+  list-style: none;
+  margin-top: 80px;
+  margin-left: 0px;
+  padding: 0px;
+  /*border: solid;*/
+}
+
+nav#menuLateral li {
+  padding-bottom: 45px;
+  /*border: solid;*/
+}
+
+nav#menuLateral img {
+  margin-right: 8px;
+}
+
+nav#menuLateral a {
+  text-decoration: none;
+  color: #5a5858;
+}
+
+nav#menuLateral a:hover {
+  text-decoration: none;
+  color: red;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,137 @@
 <template>
-  <main class="home">
-    <h1>{{ home }}</h1>
-    <form>
-      <h1>test</h1>
-    </form>
+  <main id="home">
+    <header id="cabecalho">
+      <input type="search" id="pesquisa" placeholder="Search" />
+      <input type="image" src="equilizer.png" id="btnConfig" />
+      <!--<img>-->
+      <label id="data">Data</label>
+      <!-- tag timing -->
+    </header>
+    <section class="title" id="TaskList">
+      <h1>My Task</h1>
+      <h2>Task List</h2>
+
+      <div class="highIndicator" id="divhigh">high</div>
+      <div class="mediumIndicator" id="divmedium">medium</div>
+      <div class="lowIndicator" id="divlow">low</div>
+      <ol>
+        <li class="card"></li>
+        <li class="card"></li>
+        <li class="card"></li>
+        <li class="card"></li>
+        <li class="card"></li>
+      </ol>
+    </section>
+    <section class="title" id="TaskDone">
+      <h2>Task Done</h2>
+      <ol>
+        <li class="card"></li>
+        <li class="card"></li>
+        <li class="card"></li>
+      </ol>
+    </section>
   </main>
 </template>
 
-<script lang="ts">
+<script lang='ts'>
 import Vue from 'vue';
 
 export default Vue.extend({
   name: 'Home',
+  data() {
+    return {
+
+    };
+  },
 });
 </script>
 
-<style>
+<style lang="scss">
+@charset "UTF-8";
+@import url("https://fonts.googleapis.com/css2?family=Sniglet&display=swap");
+section#home {
+  background-color: rgba(255, 255, 255, 0.3);
+  width: 1000px;
+  height: 768px;
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+  float: left;
+  margin: 10px;
+  margin-left: 0px;
+  /*opacity: 0.3   -- mudaria todos os elementos da section*/
+  /*border: solid;*/
+}
 
+header input#pesquisa {
+  //background: #fff url(search.png) no-repeat 12px;
+  border-radius: 30px;
+  border-style: hidden;
+  height: 40px;
+  width: 550px;
+  margin-top: 20px;
+  margin-left: 30px;
+  opacity: 1;
+  padding-left: 50px;
+  padding-right: 10px;
+  font-size: 15px;
+  outline-style: none;
+}
+
+header label#data {
+  float: right;
+  margin-top: 30px;
+  margin-right: 20px;
+  font-size: 13px;
+}
+
+header input#btnConfig {
+  float: right;
+  margin-top: 30px;
+  margin-right: 40px;
+  outline-style: none;
+}
+
+.title h1 {
+  margin-left: 35px;
+  font-size: 25px;
+}
+
+.title h2 {
+  margin-left: 35px;
+  font-size: 13px;
+  color: rgb(252, 87, 10);
+  /* border: solid; */
+}
+
+section#TaskList div {
+  color: white;
+  border-radius: 30px;
+  width: 50px;
+  height: 20px;
+  font-size: 11px;
+  text-align: center;
+  vertical-align: center;
+  float: right;
+  margin-right: 30px;
+}
+
+.lowIndicator {
+  background-color: #7fc7fc;
+}
+
+.mediumIndicator {
+  background-color: #f9a51a;
+}
+
+.highIndicator {
+  background-color: #f5450d;
+}
+.card {
+  background-color: white;
+  border-radius: 20px;
+  width: 180px;
+  height: 100px;
+  margin: 30px;
+  /* float: left; */
+}
 </style>
