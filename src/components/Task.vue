47c87d2c08ad4fe4102fe65a8901a2f4597849a6
hrcodes <helder.ramos.oliveira@gmail.com>
2020-12-29 09:46:54 -0300

    :art: split component task (card)

---
+++ src/components/Task.vue
@@ -0,0 +1,137 @@
+<template>
+  <ol>
+    <li v-for="(card, idx) in cards" :key="idx" :class="card.class">
+      <header class="card-item__titles card-item__spacing">
+        <h4 class="card-item__title"
+            @click="$emit('clicktitle', card.description)"
+        >
+            {{ card.title }}
+        </h4>
+        <p class="card-item__subtitle">{{ card.subtitle }}</p>
+      </header>
+      <footer class="card-item__last-line">
+        <ul class="card-item__faces card-item__spacing">
+          <li>{{ card.face1 }}</li>
+          <li>{{ card.face2 }}</li>
+          <li>{{ card.face3 }}</li>
+          <li>{{ card.face4 }}</li>
+          <li>{{ card.morefaces }}</li>
+        </ul>
+        <div :class="card.indicator"></div>
+      </footer>
+    </li>
+  </ol>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      cards: [
+        {
+          class: 'card-item',
+          title: 'Molly Moreas',
+          subtitle: 'Photo Shoot',
+          description: 'This is a card to make a photo shoot of the project and test out descriptions',
+          face1: '@',
+          face2: '@',
+          face3: '@',
+          face4: '@',
+          morefaces: '...',
+          indicator: 'indicator indicator--high card-item__indicator',
+        },
+        {
+          class: 'card-item',
+          title: 'Nike Lebron',
+          subtitle: 'Loading Page',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: 'BCA',
+          subtitle: 'Website Developer',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: 'Plainthing',
+          subtitle: '',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: '',
+          subtitle: '',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: 'Pharmacy Illustration',
+          subtitle: 'Ilustration',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: 'ESDM Video',
+          subtitle: 'Motion Graphic',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+        {
+          class: 'card-item',
+          title: 'Minerba',
+          subtitle: 'Motion Graphic',
+          description: '',
+          face1: '',
+          face2: '',
+          face3: '',
+          face4: '',
+          morefaces: '',
+          indicator: '',
+        },
+      ],
+    };
+  },
+  methods: {
+    //
+  },
+});
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,33 +12,12 @@
 
     <section class="task-list">
       <Indicator class="task-list__indicator" @selected="filter($event)" />
-      <ol class="task-list__cards">
-        <li v-for="(card, idx) in card" :key="idx" :class="card.class">
-          <header class="card-item__titles card-item__spacing">
-            <h4 class="card-item__title">{{ card.title }}</h4>
-            <p class="card-item__subtitle">{{ card.subtitle }}</p>
-          </header>
-          <footer class="card-item__last-line">
-            <ul class="card-item__faces card-item__spacing">
-              <li>{{ card.face1 }}</li>
-              <li>{{ card.face2 }}</li>
-              <li>{{ card.face3 }}</li>
-              <li>{{ card.face4 }}</li>
-              <li>{{ card.morefaces }}</li>
-            </ul>
-            <div :class="card.indicator"></div>
-          </footer>
-        </li>
-      </ol>
+      <Task class="task-list__cards" @clicktitle="showdescription($event)"/>
     </section>
 
     <section class="task-list">
       <h2 class="text text--subtitle text--red">Task Done</h2>
-      <ol class="task-list__cards">
-        <li v-for="(card, idx) in card" :key="idx" :class="card.class">
-          <h4 class="card-item__title">{{ card.title }}</h4>
-        </li>
-      </ol>
+      <Task class="task-list__cards" @clicktitle="showdescription($event)"/>
     </section>
   </main>
 </template>
@@ -46,11 +25,13 @@
 <script lang="ts">
 import Vue from 'vue';
 import Indicator from '@/components/Indicator.vue';
+import Task from '@/components/Task.vue';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     Indicator,
+    Task,
   },
   data() {
     const dtdata = new Date();
@@ -71,96 +52,6 @@ export default Vue.extend({
 
     return {
       myDate: `${months[dtdata.getMonth()]} ${dtdata.getDay()} - ${dtdata.getFullYear()}`,
-      card: [
-        {
-          class: 'card-item',
-          title: 'Molly Moreas',
-          subtitle: 'Photo Shoot',
-          face1: '@',
-          face2: '@',
-          face3: '@',
-          face4: '@',
-          morefaces: '...',
-          indicator: 'indicator indicator--high card-item__indicator',
-        },
-        {
-          class: 'card-item',
-          title: 'Nike Lebron',
-          subtitle: 'Loading Page',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: 'BCA',
-          subtitle: 'Website Developer',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: 'Plainthing',
-          subtitle: '',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: '',
-          subtitle: '',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: 'Pharmacy Illustration',
-          subtitle: 'Ilustration',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: 'ESDM Video',
-          subtitle: 'Motion Graphic',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-        {
-          class: 'card-item',
-          title: 'Minerba',
-          subtitle: 'Motion Graphic',
-          face1: '',
-          face2: '',
-          face3: '',
-          face4: '',
-          morefaces: '',
-          indicator: '',
-        },
-      ],
     };
   },
 
@@ -172,6 +63,9 @@ export default Vue.extend({
       // console.log(level);
       alert(level);
       // FILTRAR CARDS POR LEVEL
+    },
+    showdescription(description: string) {
+      alert(description);
     },
   },
 });
@@ -235,38 +129,6 @@ main#home {
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
-  }
-}
-
-.card-item {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: white;
-  border-radius: 20px;
-  height: 100px;
-  margin: 30px;
-  width: 190px;
-  padding: 15px;
-
-  &__title {
-    font-size: 13px;
-  }
-
-  &__subtitle {
-    font-size: 9px;
-    margin-top: 10px;
-  }
-
-  &__last-line {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
-
-  &__faces {
-    display: flex;
-    justify-content: space-between;
   }
 }
 
